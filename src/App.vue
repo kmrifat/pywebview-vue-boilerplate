2ed667ace0d7fa7d8823f8ae2e298367fd8c6a4d
kmrifat <kmrifat@gmail.com>
2021-04-08 20:44:02 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <input type="text">
+    <input type="password">
+    <input type="checkbox">
+    <br>
+    <button @click="myBtn">Hello</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  methods: {
+    myBtn() {
+      console.log(window.pywebview.api.takeScreenshot());
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
